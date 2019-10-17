e0038043e0ce00f83b97d72b001cf92d6a185c9c
Evgenii Vasiliev <eugene.unn@gmail.com>
2019-10-17 13:28:24 +0300

    Add TensorFlow SSD300 benchmark with batch_size=1
    Add TensorFlow SSD300 benchmark with batch_size=1. Needed future investigation.

--- tf_benchmark/tf_benchmark.py
+++ tf_benchmark/tf_benchmark.py
@@ -10,7 +10,10 @@ cd IXPUG_DNN_benchmark/tf_benchmark
 mkdir results_classification
 mkdir results_detection
 python3 tf_benchmark.py -t classification -i ../datasets/imagenet/ -m ../models/resnet-50.pb -ni 1000 -o False -of ./results_classification/ -r ./results_classification/result.csv -w 224 -he 224 -b 1
-python3 tf_benchmark.py -t detection -i ../datasets/pascal_voc/ -m ../models/ssd300.pb -ni 1000 -o False -of ./results_detection/ -r ./results_detection/result.csv -w 224 -h 224 -b 1
+python3 tf_benchmark.py -t ssd300 -i ../datasets/pascal_voc/ -m ../models/ssd_300_vgg.ckpt -ni 1000 -o False -of ./results_detection/ -r ./results_detection/result.csv -w 300 -he 300 -b 1
+
+Tensorflow realization of ResNet-50 was used from: https://docs.openvinotoolkit.org/latest/_docs_MO_DG_prepare_model_convert_model_Convert_Model_From_TensorFlow.html
+TensorFlow realization of SSD300 was used from: https://github.com/balancap/SSD-Tensorflow
 
 Created 04.10.2019
 
@@ -87,19 +90,63 @@ def write_row(filename, net_name, batch_size, number_iter, latency, total_time, 
     file.write(row + '\n')
     file.close()
 
-def load_network(model, width, height):
-    graph = tf.Graph()
-    sess = tf.InteractiveSession(graph = graph)
+def load_graph(frozen_graph_filename):
+    # We load the protobuf file from the disk and parse it to retrieve the 
+    # unserialized graph_def
+    with tf.gfile.GFile(frozen_graph_filename, "rb") as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+
+    # Then, we import the graph_def into a new Graph and returns it 
+    with tf.Graph().as_default() as graph:
+        # The name var will prefix every op/nodes in your graph
+        # Since we load everything in a new graph, this is not needed
+        tf.import_graph_def(graph_def, name="prefix")
+    return graph    
     
-    # Import the TF graph
-    with tf.gfile.GFile(model, 'rb') as f:
+def load_network(model, width, height, task_type = 'classification'):
+    
+    if task_type == 'ssd300':
+        slim = tf.contrib.slim
+        import sys
+        sys.path.append('ssd300')
+        import ssd_vgg_300
+        import ssd_vgg_preprocessing
+        # Build ssd300 model
+        isess = tf.InteractiveSession()
+
+        net_shape=(300, 300)
+        data_forma = 'NHWC'
+        input_tensor = tf.placeholder(tf.uint8, shape=(None, None, 3))
+        # Evaluation pre-processing: resize to SSD net shape.
+        image_pre, labels_pre, bboxes_pre, bbox_img = ssd_vgg_preprocessing.preprocess_for_eval(
+            input_tensor, None, None, net_shape, data_forma, resize=ssd_vgg_preprocessing.Resize.WARP_RESIZE)
+        image_4d = tf.expand_dims(image_pre, 0)
+
+        # Define the SSD model.
+        reuse = True if 'ssd_net' in locals() else None
+        ssd_net = ssd_vgg_300.SSDNet()
+        with slim.arg_scope(ssd_net.arg_scope(data_format=data_forma)):
+            predictions, localisations, _, _ = ssd_net.net(image_4d, is_training=False, reuse=reuse)
+
+        # Restore SSD model.
+        isess.run(tf.global_variables_initializer())
+        saver = tf.train.Saver()
+        saver.restore(isess, model)
+        
+        return isess, ssd_net, image_4d
+    else:
+        graph = tf.Graph()
+        sess = tf.InteractiveSession(graph = graph)
+    
+        # Import the TF graph
+        with tf.gfile.GFile(model, 'rb') as f:
             graph_def = tf.GraphDef()
             graph_def.ParseFromString(f.read())
                  
-    # Define input tensor
-    input_tensor = tf.placeholder(np.float32, shape = [None, width, height, 3], 
-                                  name='input')  
-    tf.import_graph_def(graph_def, {'input': input_tensor})
+        # Define input tensor
+        input_tensor = tf.placeholder(np.float32, shape = [None, width, height, 3], name='input') 
+        tf.import_graph_def(graph_def, {'input': input_tensor})
             
     return sess, graph, input_tensor
 
@@ -124,19 +171,49 @@ def tf_benchmark(sess, graph, input_tensor, width, height, number_iter,
     number_iter = (number_iter + batch_size - 1) // batch_size
     images, counts = load_images(width, height, input_folder, number_iter * batch_size)
  
-    output_tensor = graph.get_tensor_by_name("import/predict:0")
+    if (task_type == 'ssd300'):
+        slim = tf.contrib.slim
+        import sys
+        sys.path.append('ssd300')
+        import ssd_vgg_300
+        import ssd_vgg_preprocessing
+        net_shape=(300, 300)
+        data_forma = 'NHWC'
+        input_tensor = tf.placeholder(tf.uint8, shape=(None, None, 3))
+        image_pre, labels_pre, bboxes_pre, bbox_img = ssd_vgg_preprocessing.preprocess_for_eval(
+            input_tensor, None, None, net_shape, data_forma, resize=ssd_vgg_preprocessing.Resize.WARP_RESIZE)
+        image_4d = tf.expand_dims(image_pre, 0)
+        with slim.arg_scope(graph.arg_scope(data_format=data_forma)):
+            predictions, localisations, _, _ = graph.net(image_4d, is_training=False, reuse=True)
+    else:
+        output_tensor = graph.get_tensor_by_name("import/predict:0")
     
     #Warmup
-    blob = np.array(images[0:1])
-    output = sess.run(output_tensor, feed_dict = {input_tensor: blob})
+    if (task_type == 'ssd300'):
+        blob = np.array(images[0])
+        rimg, rpredictions, rlocalisations, rbbox_img = sess.run([input_tensor, predictions, localisations, bbox_img], feed_dict = {input_tensor: blob})
+    else:
+        blob = np.array(images[0:batch_size])
+        output = sess.run(output_tensor, feed_dict = {input_tensor: blob})
+    
     
     t0_total = time()
     for i in range(number_iter):
         a = (i * batch_size) % len(images) 
         b = (((i + 1) * batch_size - 1) % len(images)) + 1
-        blob = np.array(images[a:b])
+        
+        if (task_type == 'ssd300'):
+            blob = np.array(images[a])
+        else:
+            blob = np.array(images[a:b])
+            
         t0 = time()
-        output = sess.run(output_tensor, feed_dict = {input_tensor: blob})
+        
+        if (task_type == 'ssd300'):
+            rimg, rpredictions, rlocalisations, rbbox_img = sess.run([input_tensor, predictions, localisations, bbox_img], feed_dict = {input_tensor: blob})
+        else:
+            output = sess.run(output_tensor, feed_dict = {input_tensor: blob})
+        
         t1 = time()
     
         if (need_output == True and batch_size == 1):
@@ -156,7 +233,7 @@ def main():
     create_result_file(args.result_file)
 
     # Load network	
-    sess, graph, input_tensor = load_network(args.model, args.width, args.height)
+    sess, graph, input_tensor = load_network(args.model, args.width, args.height, args.task_type)
     
     # Execute network
     inference_time, total_time = tf_benchmark(sess, graph, input_tensor, 
